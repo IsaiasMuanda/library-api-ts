7fff66d4823f2b02804a51847bf39e3ba5697698
Isaias <ismuandel@gmail.com>
2026-04-18 02:22:48 +0100

    feat: add global error handling middleware

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,7 @@
-import "reflect-metadata";
-import express from "express";
-import "dotenv/config";
-import { connectDB } from "./config/database.ts"
-import userRoutes from "./modules/user/user.routes.ts";
-import authRoutes from "./modules/auth/auth.routes.ts";
-
-const app = express();
-
-app.use(express.json());
+import { app } from "./app";
+import { connectDB } from "./config/database";
 
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    return res.json({ message: "API rodando" });
-});
-
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
 
 async function startServer() {
     try {
