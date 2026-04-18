e6b22223974362cf2cb70d4b031eb9ea673dc913
Isaias <ismuandel@gmail.com>
2026-04-19 00:19:55 +0100

    feat: create interfaces for repository and service

--- src/modules/author/author.interface.ts
+++ src/modules/author/author.interface.ts
@@ -1,4 +1,5 @@
 import { ObjectId } from "mongoose";
+import { CreateAuthorDTO, UpdateAuthorDTO } from "./author.schema";
 
 export interface AuthorEntity {
     _id: ObjectId,
@@ -6,3 +7,20 @@ export interface AuthorEntity {
     bio?: string,
     nacionalidade?: string
 }
+
+export interface IAuthorRepository {
+    //Get all com paginação
+    findById(id: string): Promise<AuthorEntity | null>;
+    create(author: CreateAuthorDTO): Promise<AuthorEntity>
+    update(id: string, author: UpdateAuthorDTO): Promise<AuthorEntity | null>
+    delete(id: string): Promise<void>
+}
+
+export interface IAuthorService {
+    //Get all com paginação
+    getById(id: string): Promise<AuthorEntity | null>;
+    createAuthor(author: CreateAuthorDTO): Promise<AuthorEntity>
+    updateAuthor(id: string, author: UpdateAuthorDTO): Promise<AuthorEntity | null>
+    deleteAuthor(id: string): Promise<void>
+}
+
