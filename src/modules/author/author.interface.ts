e1da6150c05fc9956d3e8b81be04f51349ea87ec
Isaias <ismuandel@gmail.com>
2026-04-19 16:03:17 +0100

    feat: implement paginations on interfaces

--- src/modules/author/author.interface.ts
+++ src/modules/author/author.interface.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongoose";
 import { CreateAuthorDTO, UpdateAuthorDTO } from "./author.schema";
+import { buildPaginationOptions, buildPaginationMeta, PaginationMeta } from "../../shared/utils/pagination";
 
 export interface AuthorEntity {
     _id: ObjectId,
@@ -9,16 +10,16 @@ export interface AuthorEntity {
 }
 
 export interface IAuthorRepository {
-    //Get all com paginação
-    findById(id: string): Promise<AuthorEntity | null>;
+    findAll(opts: ReturnType<typeof buildPaginationOptions>): Promise<{ items: AuthorEntity[], total: number }>
+    findById(id: string): Promise<AuthorEntity | null>
     create(author: CreateAuthorDTO): Promise<AuthorEntity>
     update(id: string, author: UpdateAuthorDTO): Promise<AuthorEntity | null>
     delete(id: string): Promise<void>
 }
 
 export interface IAuthorService {
-    //Get all com paginação
-    getById(id: string): Promise<AuthorEntity | null>;
+    getAll(opts: ReturnType<typeof buildPaginationOptions>): Promise<{ items: AuthorEntity[]; meta: PaginationMeta }>;
+    getById(id: string): Promise<AuthorEntity | null>
     createAuthor(author: CreateAuthorDTO): Promise<AuthorEntity>
     updateAuthor(id: string, author: UpdateAuthorDTO): Promise<AuthorEntity | null>
     deleteAuthor(id: string): Promise<void>
