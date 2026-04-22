bb68ba65cc76687c321d6f4154716f60b12b2c9a
Isaias <ismuandel@gmail.com>
2026-04-22 23:49:58 +0100

    feat: create routes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import userRoutes from "./modules/user/user.routes.ts";
 import authRoutes from "./modules/auth/auth.routes.ts";
 import authorRoutes from "./modules/author/author.routes.ts"
+import bookRoutes from "./modules/book/book.routes.ts";
 import { errorMiddleware } from "./middlewares/error.middleware.ts";
 
 const app = express();
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/authors", authorRoutes);
+app.use("/api/books", bookRoutes);
 
 app.use(errorMiddleware);
 
